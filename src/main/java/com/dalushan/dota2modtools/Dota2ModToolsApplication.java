8dc0a7fc249987b5cd99dd1fb90f5acddc06fd40
lulianshan <lulianshan5417@163.com>
2021-12-17 21:20:48 +0800

    Dota2 Mod Tools Init

---
+++ src/main/java/com/dalushan/dota2modtools/Dota2ModToolsApplication.java
@@ -0,0 +1,13 @@
+package com.dalushan.dota2modtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2ModToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dota2ModToolsApplication.class, args);
+    }
+
+}
